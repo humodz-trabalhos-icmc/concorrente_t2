aa8d16f4a9963519186f2465e53a5792370bec0e
Hugo Dzin <humodz@hotmail.com>
2017-11-01 10:45:15 -0200

    scatter funcionando

--- src/main.c
+++ src/main.c
@@ -27,7 +27,7 @@ float *mat_read(FILE *fp, int *np) {
 
     for(int row = 0; row < n; row++) {
         for(int col = 0; col < n + 1; col++) {
-            fscanf(fp, " %f", &m[(n+1) * row + col]);
+            fscanf(fp, " %f", &m[n*col + row]);
         }
     }
 
@@ -38,23 +38,69 @@ float *mat_read(FILE *fp, int *np) {
 void mat_print(FILE *fp, float *m, int n) {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n + 1; j++) {
-            fprintf(fp, "%f ", m[(n+1) * i + j]);
+            fprintf(fp, "%f ", m[n*j + i]);
         }
         fprintf(fp, "\n");
     }
 }
 
 
+
+void scatter_and_print(int rank, float *m, int n) {
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    float *col = malloc(sizeof(float[n]));
+    assert(col != NULL);
+
+    int err = MPI_Scatter(
+            m, n, MPI_FLOAT,
+            col, n, MPI_FLOAT,
+            0, MPI_COMM_WORLD);
+
+    if(err != MPI_SUCCESS) {
+        printf("aff\n");
+    }
+
+    int num_procs;
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+    for(int r = 0; r < num_procs; r++) {
+        if(r == rank) {
+            printf("rank %d: ", rank);
+            for(int i = 0; i < n; i++) {
+                printf("%f ", col[i]);
+            }
+            puts("");
+            fflush(stdout);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+
+    free(col);
+}
+
+
+
 int main(int argc, char **argv) {
 
     MPI_Init(&argc, &argv);
 
+    int rank;
+    int num_procs;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
     int n;
-    float *m = mat_read(stdin, &n);
+    if(rank == 0) {
+        float *m = mat_read(stdin, &n);
 
-    mat_print(stdout, m, n);
+        mat_print(stdout, m, n);
+        scatter_and_print(rank, m, n);
 
-    mat_free(m);
+        mat_free(m);
+    } else {
+        scatter_and_print(rank, NULL, 0);
+    }
 
     MPI_Finalize();
     return 0;
