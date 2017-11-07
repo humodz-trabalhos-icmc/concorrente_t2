7debb30e396122c1088c9cf46011a33b8e0d36d0
Hugo Dzin <humodz@hotmail.com>
2017-11-07 15:32:47 -0200

    add: escolher até qual tamanho criar

--- make_inputs.py
+++ make_inputs.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 
+if len(sys.argv) > 1:
+    until_which = int(sys.argv[1])
+else:
+    until_which = None
 
 # Os 3 primeiros pra ser mais facil de testar
 dims = [10, 20, 40, 1000, 5000, 10000]
+dims = dims[:until_which]
 
 
 for n in dims:
+    print('Creating size', n)
     path = 'input/' + str(n)
     try:
         os.makedirs(path)
@@ -20,7 +27,7 @@ for n in dims:
             if (i + 1) % n == 0:
                 end = '\n'
 
-            print(i, file=matfile, end='')
+            print(i, file=matfile, end=end)
 
     with open(path + '/vetor.txt', 'w') as vecfile:
         for i in range(n):
