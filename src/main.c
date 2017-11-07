ea6ac889ede5286730d15342d8a180be89db4be3
Hugo Dzin <humodz@hotmail.com>
2017-11-07 16:42:42 -0200

    add: scatter ciclico em colunas

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@
 #include "matrix.h"
 
 
-void scatter_and_print(int rank, float *m, int n) {
+void scatter_and_print_old(int rank, float *m, int n) {
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
     float *col = malloc(sizeof(float[n]));
     assert(col != NULL);
@@ -44,6 +44,54 @@ void scatter_and_print(int rank, float *m, int n) {
 }
 
 
+void scatter_and_print(ColMajorMatrix *mat, int rank, int num_procs) {
+    unsigned long long int n;
+    float *send_buf = NULL;
+
+    if(rank == 0) {
+        n = mat->n;
+        send_buf = mat->data;
+    }
+
+    MPI_Bcast(&n, 1, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
+
+
+    size_t cols_per_process = n / num_procs;
+    float *recv_buf = malloc(sizeof(float[n * cols_per_process]));
+    assert(recv_buf != NULL);
+
+    // Send columns in rounds e.g. A B C A B C
+    float *recv_buf_aux = recv_buf;
+    for(size_t round = 0; round < cols_per_process; round++) {
+
+        MPI_Scatter(
+                send_buf, n, MPI_FLOAT,
+                recv_buf_aux, n, MPI_FLOAT,
+                0, MPI_COMM_WORLD);
+
+        send_buf += n * num_procs;  // Skip to next block
+        recv_buf_aux += n; // Skip to next column;
+    }
+
+    for(int round = 0; round < num_procs; round++) {
+        if(round == rank) {
+            printf("rank %d:\n", rank);
+
+            for(size_t col = 0; col < cols_per_process; col++) {
+                printf("  ");
+                for(size_t row = 0; row < n; row++) {
+                    printf("%f ", recv_buf[n*col + row]);
+                }
+                printf("\n");
+            }
+            fflush(stdout);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+
+    free(recv_buf);
+}
+
 
 int main(int argc, char **argv) {
 
@@ -69,20 +117,17 @@ int main(int argc, char **argv) {
         Vector vec = Vector_read(vector_file);
         ColMajorMatrix mat = Matrix_read(matrix_file, vec.n);
 
-        Vector_print(&vec, stdout);
-        Matrix_print(&mat, stdout);
-
         fclose(vector_file);
         fclose(matrix_file);
+
+
+        scatter_and_print(&mat, rank, num_procs);
 
         Vector_free(&vec);
         Matrix_free(&mat);
 
-
-        //scatter_and_print(rank, m, n);
-
     } else {
-        //scatter_and_print(rank, NULL, 0);
+        scatter_and_print(NULL, rank, num_procs);
     }
 
     MPI_Finalize();
